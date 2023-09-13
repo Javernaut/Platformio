b5c10cfbda4b09912b44d7733d179ccc82df150f
javernaut <javernaut@gmail.com>
2023-09-13 18:37:04 +0200

    Running left and right

--- Assets/_Project/Scripts/Platformio/Player/PlayerMovement.cs
+++ Assets/_Project/Scripts/Platformio/Player/PlayerMovement.cs
@@ -6,8 +6,27 @@ namespace Platformio.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        private Vector2 moveInput;
+        [SerializeField] private float runSpeed = 10f;
         
+        private Vector2 moveInput;
+        private Rigidbody2D _myRigidbody;
+
+        private void Awake()
+        {
+            _myRigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            Run();
+        }
+
+        private void Run()
+        {
+            var playerVelocity = new Vector2(moveInput.x * runSpeed, _myRigidbody.velocity.y);
+            _myRigidbody.velocity = playerVelocity;
+        }
+
         private void OnMove(InputValue inputValue)
         {
             moveInput = inputValue.Get<Vector2>();
