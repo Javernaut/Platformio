66fd829250930e7a255f67ac2ec58f5e5b316a15
javernaut <javernaut@gmail.com>
2023-09-13 18:55:36 +0200

    Basic jump

--- Assets/_Project/Scripts/Platformio/Player/PlayerMovement.cs
+++ Assets/_Project/Scripts/Platformio/Player/PlayerMovement.cs
@@ -7,13 +7,16 @@ namespace Platformio.Player
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private float runSpeed = 10f;
+        [SerializeField] float jumpSpeed = 5f;
         
         private Vector2 moveInput;
         private Rigidbody2D _myRigidbody;
+        private Animator _myAnimator;
 
         private void Awake()
         {
             _myRigidbody = GetComponent<Rigidbody2D>();
+            _myAnimator = GetComponent<Animator>();
         }
 
         private void Update()
@@ -26,12 +29,25 @@ namespace Platformio.Player
         {
             var playerVelocity = new Vector2(moveInput.x * runSpeed, _myRigidbody.velocity.y);
             _myRigidbody.velocity = playerVelocity;
+            
+            var playerHasHorizontalSpeed = Mathf.Abs((_myRigidbody).velocity.x) > Mathf.Epsilon;
+            _myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
         }
 
         private void OnMove(InputValue inputValue)
         {
             moveInput = inputValue.Get<Vector2>();
         }
+        
+        void OnJump(InputValue value)
+        {
+            if(value.isPressed)
+            {
+                // TODO Shouldn't it be applyForce(impulse)?
+                _myRigidbody.velocity += new Vector2 (0f, jumpSpeed);
+            }
+        }
+
 
         private void FlipSprite()
         {
