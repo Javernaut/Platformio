6080d2ef26ec5e9ad8895121d09df20ab2bf94fb
javernaut <javernaut@gmail.com>
2023-09-14 22:36:34 +0200

    Adding a simple enemy

---
+++ Assets/_Project/Scripts/Platformio/Player/EnemyMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace Platformio.Player
+{
+    public class EnemyMovement : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 1f;
+        private Rigidbody2D _myRigidbody;
+    
+        void Start()
+        {
+            _myRigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        void Update()
+        {
+            _myRigidbody.velocity = new Vector2 (moveSpeed, 0f);
+        }
+
+        void OnTriggerExit2D(Collider2D other) 
+        {
+            moveSpeed = -moveSpeed;
+            FlipEnemyFacing();
+        }
+
+        void FlipEnemyFacing()
+        {
+            transform.localScale = new Vector2 ((Mathf.Sign(_myRigidbody.velocity.x)), 1f);
+        }
+
+    }
+}
