fc1af42616861120185862ee3a8b152ea2e5ee80
javernaut <javernaut@gmail.com>
2023-09-16 21:17:19 +0200

    Playing sound on a coin pickup

--- Assets/_Project/Scripts/Platformio/Pickup/Coin.cs
+++ Assets/_Project/Scripts/Platformio/Pickup/Coin.cs
@@ -4,10 +4,13 @@ namespace Platformio.Pickup
 {
     public class Coin : MonoBehaviour
     {
+        [SerializeField] private AudioClip coinPickupSFX;
+        
         void OnTriggerEnter2D(Collider2D other) 
         {
             if (other.CompareTag("Player"))
             {
+                AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
                 Destroy(gameObject);
             }
         }
