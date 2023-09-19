41a5d0ad58a366c1e01ed175db2e97d3d064718a
javernaut <javernaut@gmail.com>
2023-09-19 17:01:44 +0200

    Creating a single scene for all levels

---
+++ Assets/_Project/Scripts/Platformio/DI/GameLoopSceneInstaller.cs
@@ -0,0 +1,11 @@
+using Zenject;
+
+namespace Platformio.DI
+{
+    public class GameLoopSceneInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
