e51cbe9dc469f2843cfaae7dde8a992b8409c442
javernaut <javernaut@gmail.com>
2023-10-04 14:06:53 +0200

    Randomizing the themes applying

--- Assets/_Project/Scripts/Platformio/Level/LevelGenerator.cs
+++ Assets/_Project/Scripts/Platformio/Level/LevelGenerator.cs
@@ -12,8 +12,6 @@ namespace Platformio.Level
     {
         [Inject] private readonly Settings _settings;
 
-        private int _currentThemeIndex;
-
         public Object GetLevelPrefab(InjectContext context)
         {
             return _settings.levelPrefabs.GetRandomItem();
@@ -21,16 +19,11 @@ namespace Platformio.Level
 
         public void InjectLevelGameObject(DiContainer container)
         {
-            var theme = _settings.themes[_currentThemeIndex++];
+            var theme = _settings.themes.GetRandomItem();
             
             container.BindInstance(theme);
             container.BindInstance(theme.stepsSounds).WhenInjectedInto<StepSoundPlayer>();
             container.BindInterfacesAndSelfTo<StepSoundPlayer>().AsSingle();
-
-            if (_currentThemeIndex >= _settings.themes.Length)
-            {
-                _currentThemeIndex = 0;
-            }
         }
 
         [Serializable]
